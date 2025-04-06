c27602ac90d6767cb798391dee86d50ff0566507
lijinchun <jcli@tendyron.com>
2025-04-06 12:43:07 +0800

    增加事件设置、等待及其他功能

--- yos/include/yos_httpd.h
+++ yos/include/yos_httpd.h
@@ -25,8 +25,7 @@ typedef int32_t(*yos_httpd_uri_handler_t)(void* req);
  *
  * @return NULL failure/other success
  */
-yos_httpd_handle_t yos_httpd_create(uint16_t server_port, const char* uri,  yos_httpd_uri_handler_t uri_handler);
-
+yos_httpd_handle_t yos_httpd_create(uint16_t server_port);
 
 /**
  * @brief Destroy client instance
@@ -36,6 +35,75 @@ yos_httpd_handle_t yos_httpd_create(uint16_t server_port, const char* uri,  yos_
  * @return 0 success/other failure
  */
 int32_t yos_httpd_destory(yos_httpd_handle_t server);
+
+
+/**
+ * @brief Registers a URI handler with the HTTP server.
+ *
+ * This function allows you to associate a specific URI with a handler function
+ * that will be invoked when the URI is accessed. You can also pass user-defined
+ * data to the handler.
+ *
+ * @param server The handle to the HTTP server instance.
+ * @param uri The URI string to register. This should be a null-terminated string.
+ * @param uri_handler The callback function to handle requests to the specified URI.
+ * @param udata A pointer to user-defined data that will be passed to the handler.
+ *
+ * @return 
+ *      - 0 on success.
+ *      - A negative value on failure.
+ */
+int32_t yos_register_uri_handler(yos_httpd_handle_t server, const char* uri, yos_httpd_uri_handler_t uri_handler, void* udata);
+
+
+/**
+ * @brief Unregisters a URI handler from the HTTP server.
+ *
+ * This function removes a previously registered URI handler from the HTTP server.
+ * It stops the server from invoking the specified handler for the given URI.
+ *
+ * @param server The handle to the HTTP server instance.
+ * @param uri The URI string for which the handler is to be unregistered.
+ * 
+ * @return 
+ *      - 0 on success.
+ *      - Negative value on failure.
+ */
+int32_t yos_unregister_uri_handler(yos_httpd_handle_t server, const char* uri);
+
+
+/**
+ * @brief Waits for specific bits to be set in the HTTP server's event flags.
+ *
+ * This function blocks the calling thread until the specified bits are set in the
+ * event flags of the HTTP server or the timeout period elapses.
+ *
+ * @param server The handle to the HTTP server instance.
+ * @param value The bitmask specifying the bits to wait for.
+ * @param ms The timeout period in milliseconds. If set to 0, the function will not block.
+ * 
+ * @return 
+ *     - A positive value indicating the bits that were set.
+ *     - 0 if the timeout period elapsed without the specified bits being set.
+ *     - A negative value if an error occurred.
+ */
+int32_t yos_httpd_wait_bits(yos_httpd_handle_t server, uint32_t value, uint32_t ms);
+
+
+/**
+ * @brief Sets specific bits in the HTTP server configuration.
+ *
+ * This function allows modifying the configuration of the HTTP server
+ * by setting specific bits in the provided value.
+ *
+ * @param server The handle to the HTTP server instance.
+ * @param value  The bitmask value to set in the server configuration.
+ *
+ * @return A 32-bit integer indicating the result of the operation.
+ *         Typically, 0 indicates success, while a negative value
+ *         indicates an error.
+ */
+int32_t yos_httpd_set_bits(yos_httpd_handle_t server, uint32_t value);
 
 
 /**
@@ -49,7 +117,30 @@ int32_t yos_httpd_destory(yos_httpd_handle_t server);
  *         The returned string is managed internally and should not be modified or freed.
  */
 const char* yos_httpd_req_get_method(void* req);
+    
 
+/**
+ * @brief Retrieves the HTTP server handle associated with a given request.
+ *
+ * This function extracts and returns the handle of the HTTP server
+ * that is managing the specified request.
+ *
+ * @param req A pointer to the HTTP request object.
+ * @return The handle of the HTTP server managing the request.
+ */
+yos_httpd_handle_t yos_httpd_req_get_handle(void* req);
+
+
+/**
+ * @brief Retrieves the user data associated with the given HTTP request.
+ *
+ * This function extracts and returns the user data pointer from the provided HTTP request object.
+ *
+ * @param req A pointer to the HTTP request object.
+ * @return A void pointer to the user data associated with the request.
+ *         The returned pointer is managed internally and should not be modified or freed.
+ */
+void* yos_httpd_req_get_udata(void* req);
 
 /**
  * @brief Retrieves the URI of the given HTTP request.
@@ -60,7 +151,7 @@ const char* yos_httpd_req_get_method(void* req);
  * @return A constant character pointer to the URI string.
  *         The returned string is managed internally and should not be modified or freed.
  */
-const char* yos_httpd_req_get_uri(void* req);
+const char* yos_httpd_req_get_uri(void* req, uint32_t* out_len);
 
 
 /**
--- yos/src_esp/yos_httpd.c
+++ yos/src_esp/yos_httpd.c
@@ -11,34 +11,73 @@
 static const char *TAG = "YOS_HTTPD";
 
 
-yos_httpd_handle_t yos_httpd_create(uint16_t server_port, const char* uri, yos_httpd_uri_handler_t uri_handler)
+static void _user_ctx_free(void* arg)
+{
+    vEventGroupDelete((EventGroupHandle_t)arg);
+}
+
+yos_httpd_handle_t yos_httpd_create(uint16_t server_port)
 {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     httpd_handle_t server = NULL;
     config.server_port = server_port;
+    config.global_user_ctx = xEventGroupCreate();
+    config.global_user_ctx_free_fn = _user_ctx_free;
     if (httpd_start(&server, &config) != ESP_OK) {
         ESP_LOGE(TAG, "Failed to httpd_start, server_port: %d", server_port);
         return NULL;
     }
 
-    if (uri_handler != NULL){
-        const httpd_uri_t uri_get = {
-            .uri = uri,
-            .method = HTTP_ANY,
-            .handler = (esp_err_t (*)(httpd_req_t *))uri_handler,
-            .user_ctx = NULL
-        };
-        if (httpd_register_uri_handler(server, &uri_get) != ESP_OK){
-            ESP_LOGE(TAG, "Failed to httpd_register_uri_handler");
-        }
-    }
- 
     return server;
 }
 
 int32_t yos_httpd_destory(yos_httpd_handle_t server)
 {
+    if (server == NULL){
+        return -1;
+    }    
+    //vEventGroupDelete((EventGroupHandle_t)httpd_get_global_user_ctx(server));
     return (int32_t)httpd_stop(server);
+}
+
+int32_t yos_register_uri_handler(yos_httpd_handle_t server, const char* uri, yos_httpd_uri_handler_t uri_handler, void* udata)
+{
+    if (server == NULL || uri == NULL || uri_handler == NULL){
+        return -1;
+    }
+    const httpd_uri_t uri_get = {
+        .uri = uri,
+        .method = HTTP_ANY,
+        .handler = (esp_err_t (*)(httpd_req_t *))uri_handler,
+        .user_ctx = udata
+    };
+    return (int32_t)httpd_register_uri_handler(server, &uri_get);
+}
+
+int32_t yos_unregister_uri_handler(yos_httpd_handle_t server, const char* uri)
+{
+    if (server == NULL || uri == NULL){
+        return -1;
+    }
+    return (int32_t)httpd_unregister_uri(server, uri);
+}
+
+int32_t yos_httpd_wait_bits(yos_httpd_handle_t server, uint32_t value, uint32_t ms)
+{
+    ESP_LOGI(TAG, "wait bits= %lx, ms= %d", value, (int)ms);
+    EventGroupHandle_t event_group = (EventGroupHandle_t)httpd_get_global_user_ctx(server);
+
+    EventBits_t bits = xEventGroupWaitBits(event_group, value, pdFALSE, pdFALSE, pdMS_TO_TICKS(ms));
+
+    ESP_LOGI(TAG, "wait bits end");
+    return (bits & BIT0)? 0 : -1;
+}
+
+int32_t yos_httpd_set_bits(yos_httpd_handle_t server, uint32_t value)
+{
+    ESP_LOGI(TAG, "set bits: %lx", value);
+    EventGroupHandle_t event_group = (EventGroupHandle_t)httpd_get_global_user_ctx(server);
+    return xEventGroupSetBits(event_group, value);
 }
 
 const char* yos_httpd_req_get_method(void* req)
@@ -50,9 +89,34 @@ const char* yos_httpd_req_get_method(void* req)
     return (method == HTTP_GET)? "GET" : "POST";
 }
 
-const char* yos_httpd_req_get_uri(void* req)
+yos_httpd_handle_t yos_httpd_req_get_handle(void* req)
 {
-    return (req != NULL)? ((httpd_req_t*)req)->uri : NULL;
+    if (req == NULL){
+        return NULL;
+    }
+    return (yos_httpd_handle_t)((httpd_req_t*)req)->handle;
+}
+
+void* yos_httpd_req_get_udata(void* req)
+{
+    if (req == NULL){
+        return NULL;
+    }
+    return ((httpd_req_t*)req)->user_ctx;
+}
+
+
+const char* yos_httpd_req_get_uri(void* req, uint32_t* out_len)
+{
+    if (req == NULL){
+        return NULL;
+    } 
+    const char* uri = ((httpd_req_t*)req)->uri;
+    if ((uri != NULL) && (out_len != NULL)){
+        *out_len = strlen(uri);
+    }
+    ESP_LOGI(TAG, "uri: %s", uri);
+    return uri;
 }
 
 int32_t yos_httpd_resp_send(void *req, const char *buf, uint32_t buf_len)
